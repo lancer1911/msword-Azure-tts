962a629bebfdab98ca78c711ec23c6b5c783cdaa
lancer1911 <39460817+lancer1911@users.noreply.github.com>
2023-05-14 23:51:28 +0800

    Add files via upload

--- msword-Azure-tts.py
+++ msword-Azure-tts.py
@@ -11,6 +11,7 @@ import configparser
 import azure.cognitiveservices.speech as speechsdk
 from tkinter import filedialog
 from tkinter import Tk
+from pydub import AudioSegment
 
 
 def docx_to_text(file_path):
@@ -59,7 +60,33 @@ else:
 # Convert Word document to text
 text = docx_to_text(file_path)
 
-# Convert text to speech and save as MP3
+# Determine split length based on language
+split_length = 2500 if speech_recognition_language == "zh-CN" else 2000
+
+# Split text into smaller pieces
+texts = [text[i:i + split_length] for i in range(0, len(text), split_length)]
+
+# Convert each piece of text to speech and save as separate MP3 files
 input_filename_without_ext, _ = os.path.splitext(os.path.basename(file_path))
-output_filename = f"{input_filename_without_ext}-Azure-tts.mp3"
-text_to_speech(text, output_filename, subscription_key, region, voice_shortname, speech_recognition_language)
+output_filenames = []
+for i, text in enumerate(texts):
+    output_filename = f"{input_filename_without_ext}-Azure-tts-part{i}.mp3"
+    text_to_speech(text, output_filename, subscription_key, region, voice_shortname, speech_recognition_language)
+    output_filenames.append(output_filename)
+
+# Combine all MP3 files into one
+combined = AudioSegment.empty()
+for filename in output_filenames:
+    sound = AudioSegment.from_mp3(filename)
+    combined += sound
+
+# Save the combined audio to a file
+combined_filename = f"{input_filename_without_ext}-Azure-tts.mp3"
+combined.export(combined_filename, format="mp3")
+
+# Optionally, delete the individual MP3 files
+for filename in output_filenames:
+    os.remove(filename)
+
+print(f"All parts combined and saved to {combined_filename}")
+
